bbb36efdeb7da9d9849fa876845e6e1ac604e3d9
zombidust <kalo.stoykov@gmail.com>
2015-03-30 20:38:21 +0300

    Adding Api entities

---
+++ Yoyo_Event_Forum/src/com/yoyoeventforum/model/Post.java
@@ -0,0 +1,62 @@
+package com.yoyoeventforum.model;
+
+public class Post {
+	
+	private long id;
+	private User author;
+	private String type;
+	private String title;
+	private String place;
+	private String date;
+	private String time;
+	private String description;
+	
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public User getAuthor() {
+		return author;
+	}
+	public void setAuthor(User author) {
+		this.author = author;
+	}
+	public String getType() {
+		return type;
+	}
+	public void setType(String type) {
+		this.type = type;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getPlace() {
+		return place;
+	}
+	public void setPlace(String place) {
+		this.place = place;
+	}
+	public String getDate() {
+		return date;
+	}
+	public void setDate(String date) {
+		this.date = date;
+	}
+	public String getTime() {
+		return time;
+	}
+	public void setTime(String time) {
+		this.time = time;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+}
